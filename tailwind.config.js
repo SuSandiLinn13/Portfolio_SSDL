98dac3fb937d577431aa39e1a529117fc3a16995
SuSandiLinn13 <99651304+SuSandiLinn13@users.noreply.github.com>
2023-10-20 19:08:28 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "main-blue": "#16175a",
+      },
+    },
+  },
+  plugins: [],
+};
